0dab42cd6356b607699c53418c3e10c5b69504f8
Matheus Melo <matheus@santins.com.br>
2022-05-28 15:06:22 -0300

    V1.2

--- assets/js/menuMobile.js
+++ assets/js/menuMobile.js
@@ -1,26 +1,21 @@
-let menutoggle = document.querySelector('.toggle')
-let showmenu = document.querySelector('nav')
+let menuToggle = document.querySelector(".toggle");
+let menu = document.querySelector("nav");
 
-menutoggle.addEventListener('click', () => {
-    menutoggle.classList.toggle('active')
-    showmenu.classList.toggle('show')
-})
+if (window.innerWidth <= 800) {
+    
+    menuToggle.onclick = () => {
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("show");
 
-// let menuToggle = document.querySelector(".toggle");
-//   let menu = document.querySelector(".desk-menu");
+};
 
-//   if (window.innerWidth < 800) {
-//     menuToggle.onclick = () => {
-//       menuToggle.classList.toggle("active");
-//       menu.classList.toggle("show");
-//     };
+let menuList = document.querySelectorAll("nav ul li");
+    menuList.forEach((list) => {
+        list.addEventListener("click", (e) => {
 
-//     let menuList = document.querySelectorAll(".desk-menu li");
+            menuToggle.classList.toggle("active");
+            menu.classList.toggle("show");
 
-//     menuList.forEach((list) => {
-//       list.addEventListener("click", (e) => {
-//         menuToggle.classList.toggle("active");
-//         menu.classList.toggle("show");
-//       });
-//     });
-//   }
+        });
+    });
+}
